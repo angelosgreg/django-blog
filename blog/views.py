1be22c9140839ccdbf25868c669173ddd997cb32
Angelos Grigorakis <angelosgreg13@gmail.com>
2023-12-08 15:48:29 +0100

    views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
 
-# Create your views here.
+
+from django.shortcuts import render
+from blog.models import Post, Comment
+
+def blog_index(request):
+    posts = Post.objects.all().order_by("-created_on")
+    context = {
+        "posts": posts,
+    }
+    return render(request, "blog/index.html", context)
